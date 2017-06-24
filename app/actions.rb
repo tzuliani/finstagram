a570f444b39f73a2fee075bf879023b0ed90c763
Theresa Zuliani <theresa.zuliani@gmail.com>
2017-06-24 18:09:20 +0000

    added erb

--- app/actions.rb
+++ app/actions.rb
@@ -9,7 +9,7 @@ end
 get '/' do
     #...
     
-    post_shark = {
+    @post_shark = {
         username: "sharky_j",
         avatar_url: "http://naserca.com/images/sharky_j.jpg",
         photo_url: "http://naserca.com/images/shark.jpg",
@@ -22,7 +22,7 @@ get '/' do
     }]
     }
     
-    post_whale = {
+    @post_whale = {
         username: "kirk_whalum",
         avatar_url: "http://naserca.com/images/kirk_whalum.jpg",
         photo_url: "http://naserca.com/images/whale.jpg",
@@ -35,7 +35,7 @@ get '/' do
     }]
     }
 
-    post_marlin = {
+    @post_marlin = {
         username: "marlin_peppa",
         avatar_url: "http://naserca.com/images/marlin_peppa.jpg",
         photo_url: "http://naserca.com/images/marlin.jpg",
@@ -47,6 +47,10 @@ get '/' do
             text: "lunch time! ;)"
     }]
     }
-     [post_shark, post_whale, post_marlin].to_s
+     [@post_shark, @post_whale, @post_marlin].to_s
+     
+     @posts = [@post_shark, @post_whale, @post_marlin]
+     
+     erb :index
 end 
 
