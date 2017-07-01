dd2691b5e9694611acaf4fbc130535e1a259850f
Theresa Zuliani <theresa.zuliani@gmail.com>
2017-07-01 16:52:32 +0000

    added forms and validations

--- app/actions.rb
+++ app/actions.rb
@@ -7,50 +7,30 @@ def humanized_time_ago(time_ago_in_minutes)
 end
 
 get '/' do
-    #...
-    
-    @post_shark = {
-        username: "sharky_j",
-        avatar_url: "http://naserca.com/images/sharky_j.jpg",
-        photo_url: "http://naserca.com/images/shark.jpg",
-        humanized_time_ago: humanized_time_ago(15),
-        like_count: 0,
-        comment_count: 1,
-        comments: [{
-            username:"sharky_j",
-            text: "Out for the long weekend... too embarrassed to show y'all the beach bod!"
-    }]
-    }
-    
-    @post_whale = {
-        username: "kirk_whalum",
-        avatar_url: "http://naserca.com/images/kirk_whalum.jpg",
-        photo_url: "http://naserca.com/images/whale.jpg",
-        humanized_time_ago: humanized_time_ago(65),
-        like_count: 0,
-        comment_count: 1,
-        comments: [{
-            username:"kirk_whalum",
-            text: "#weekendvibes"
-    }]
-    }
+    @posts = Post.order(created_at: :desc)
+    erb(:index)
+end
 
-    @post_marlin = {
-        username: "marlin_peppa",
-        avatar_url: "http://naserca.com/images/marlin_peppa.jpg",
-        photo_url: "http://naserca.com/images/marlin.jpg",
-        humanized_time_ago: humanized_time_ago(190),
-        like_count: 0,
-        comment_count: 1,
-        comments: [{
-            username:"marlin_peppa",
-            text: "lunch time! ;)"
-    }]
-    }
-     [@post_shark, @post_whale, @post_marlin].to_s
-     
-     @posts = [@post_shark, @post_whale, @post_marlin]
-     
-     erb :index
-end 
+get '/signup' do        # if a user navigates to the path "signup",
+    @user = User.new    # setup empty @user object
+    erb(:signup)        # render "app/views/signup.erb"
+end
+    
+post '/signup' do
+    # grab user input values from params
+    email       = params[:email]
+    avatar_url  = params[:avatar_url]
+    username    = params[:username]
+    password    = params[:password]
+    
+    @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password})
+    
+    if @user.save
+        "User #{username} saved!"
+    else
+        erb(:signup)
+        
+    end
+    
+end
 
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,25 @@
 class Post < ActiveRecord::Base
     
-end 
+    belongs_to :user
+    has_many :comments
+    has_many :likes
+    
+    def humanized_time_ago
+        time_ago_in_seconds = Time.now - self.created_at
+        time_ago_in_minutes = time_ago_in_seconds / 60
+        
+    if time_ago_in_minutes >= 60
+            "#{(time_ago_in_minutes / 60).to_i} hours ago"
+        else
+            "#{time_ago_in_minutes.to.i} minutes ago"
+    end
+        
+        def like_count
+            self.likes.size
+        end
+        
+        def comment_count
+            self.comments.size
+        end
+    end
+end
